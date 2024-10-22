dec2ec30f8a9bca160325abe83387615387984ee
xyz100zyx <xyz100zyx@mail.ru>
2024-10-22 22:47:06 +0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import eslintPrettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import parser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': eslintReactHooks,
+      'eslint-react': eslintReact,
+      'eslint-react-refresh': eslintReactRefresh,
+      prettier: eslintPrettierPlugin,
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        ...eslintReact.configs.flat.recommended.languageOptions.parserOptions,
+        project: ['tsconfig.json', 'tsconfig.node.json', 'tsconfig.app.json'],
+      },
+    },
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      semi: 'error',
+      'prefer-const': 'error',
+      ...eslintConfigPrettier.rules,
+    },
+  },
+  { files: ['**/*.{js,jsx,ts,tsx}'] },
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+);
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const b = 2;
+
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
